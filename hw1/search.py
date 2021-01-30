3a06dd4cb88b88fa8b00a9252ee9c3970aabc87b
keelimepi <freddy.z1999@gmail.com>
2021-01-30 11:28:21 -0500

    initial addition of assignment 1

---
+++ hw1/search.py
@@ -0,0 +1,73 @@
+# search.py
+# ---------------
+# Licensing Information:  You are free to use or extend this projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to the University of Illinois at Urbana-Champaign
+#
+# Created by Kelvin Ma (kelvinm2@illinois.edu) on 01/24/2021
+
+"""
+This is the main entry point for MP1. You should only modify code
+within this file -- the unrevised staff files will be used for all other
+files and classes when code is run, so be careful to not modify anything else.
+"""
+# Search should return the path.
+# The path should be a list of tuples in the form (row, col) that correspond
+# to the positions of the path taken by your search algorithm.
+# maze is a Maze object based on the maze from the file specified by input filename
+# searchMethod is the search method specified by --method flag (bfs,dfs,astar,astar_multi,fast)
+
+
+def bfs(maze):
+    """
+    Runs BFS for part 1 of the assignment.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    return []
+
+def astar_single(maze):
+    """
+    Runs A star for part 2 of the assignment.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    return []
+
+def astar_corner(maze):
+    """
+    Runs A star for part 3 of the assignment in the case where there are four corner objectives.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+        """
+    return []
+
+def astar_multiple(maze):
+    """
+    Runs A star for part 4 of the assignment in the case where there are
+    multiple objectives.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    return []
+
+def fast(maze):
+    """
+    Runs suboptimal search algorithm for part 5.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    return []
+    
+            
