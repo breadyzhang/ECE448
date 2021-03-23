46631e91696eb59b5c824b22ce7a6bf2ec8be142
zhangyfreddy <freddy.z1999@gmail.com>
2021-03-23 13:21:25 -0500

    finished part1

--- hw4/baseline.py
+++ hw4/baseline.py
@@ -12,7 +12,7 @@
 """
 Part 1: Simple baseline that only uses word statistics to predict tags
 """
-
+from collections import Counter
 def baseline(train, test):
     '''
     input:  training data (list of sentences, with tags on the words)
@@ -20,5 +20,41 @@ def baseline(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    
-    return []
+    out = []
+    words = {} # (freq of tag, tag)
+    word_tag = {}
+    tags = Counter()
+    # tags = {} # (freq of word, word)
+    # training
+    for message in train:
+        for word in message:
+            if word[0] not in words:
+                words[word[0]] = [word[1]]
+                word_tag[(word[0],word[1])] = 1
+            elif word[1] not in words[word[0]]:
+                words[word[0]].append(word[1])
+                word_tag[(word[0],word[1])] = 1
+            else:
+                word_tag[(word[0],word[1])] += 1
+            tags[word[1]] += 1
+        # if word[1] not in tags:
+        #     tags[word[1]] = (1, word[0])
+        # else:
+        #     tags[word[1]] = (tags[word[1]][0] + 1, word[0])
+
+    # testing
+    for message in test:
+        message_out = []
+        for word in message:
+            best_tag = ""
+            value = 0
+            if word in words:
+                for tag in words[word]:
+                    if word_tag[(word,tag)] > value:
+                        value = word_tag[(word,tag)]
+                        best_tag = tag
+            else:
+                best_tag = max(tags)
+            message_out.append((word,best_tag))
+        out.append(message_out)
+    return out
